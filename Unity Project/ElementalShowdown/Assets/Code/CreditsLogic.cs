d33968b773634c22202099467676624be7a345cd
Jethro Schoppenhorst <38409262+JSchoppe@users.noreply.github.com>
2019-04-12 13:17:42 -0500

    Important Scenes Created

---
+++ Unity Project/ElementalShowdown/Assets/Code/CreditsLogic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsLogic : MonoBehaviour
+{
+    [SerializeField]
+    private string sceneName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+        }
+    }
+}
---
+++ Unity Project/ElementalShowdown/Assets/Code/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Startup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Load the main menu
+        //SceneManager.LoadScene()
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
