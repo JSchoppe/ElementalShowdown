e83859a41c3f306a76f855094087d4bac8167977
Jethro Schoppenhorst <38409262+JSchoppe@users.noreply.github.com>
2019-04-12 11:46:01 -0500

    Basic Meters Implemented

---
+++ Unity Project/ElementalShowdown/Assets/Code/PlayerMeter.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMeter : MonoBehaviour
+{
+    [SerializeField]
+    private Texture fullWand;
+    [SerializeField]
+    private Texture drainedWand;
+
+    [SerializeField]
+    private Slider healthSlider;
+    [SerializeField]
+    private RawImage[] wandCharges;
+
+
+    public void UpdateWandCharges(int newCharges)
+    {
+        for (int i = 0; i < wandCharges.Length; i++)
+        {
+            if (i < newCharges) // This charge is filled.
+            {
+                wandCharges[i].texture = fullWand;
+            }
+            else
+            {
+                wandCharges[i].texture = drainedWand;
+            }
+        }
+    }
+
+    public void UpdateHealth(float newHealth)
+    {
+        healthSlider.value = newHealth;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Debug:
+    //int currentWand;
+    //private void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.LeftArrow))
+    //    {
+    //        currentWand--;
+    //        if(currentWand < 0)
+    //        {
+    //            currentWand = 0;
+    //        }
+    //    }
+    //    else if (Input.GetKeyDown(KeyCode.RightArrow))
+    //    {
+    //        currentWand++;
+    //        if (currentWand > 5)
+    //        {
+    //            currentWand = 5;
+    //        }
+    //    }
+    //    UpdateWandCharges(currentWand);
+    //}
+}
