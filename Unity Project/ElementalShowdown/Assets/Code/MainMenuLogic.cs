45a2f5385c479bdf521b5490f456092b2e524f30
Jethro Schoppenhorst <38409262+JSchoppe@users.noreply.github.com>
2019-04-13 13:25:14 -0500

    main menu logic

---
+++ Unity Project/ElementalShowdown/Assets/Code/MainMenuLogic.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuLogic : MonoBehaviour
+{
+    [SerializeField]
+    private Button startButton;
+    [SerializeField]
+    private Button instructButton;
+    [SerializeField]
+    private Button creditsButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Startup.ChangeAudioTrack(Startup.Track.Main);
+
+        startButton.onClick.AddListener(startClick);
+        instructButton.onClick.AddListener(startClick);
+        creditsButton.onClick.AddListener(startClick);
+    }
+
+
+    void startClick()
+    {
+        SceneManager.LoadScene("SamScene", LoadSceneMode.Single);
+    }
+    void instructClick()
+    {
+        SceneManager.LoadScene("Instructions", LoadSceneMode.Single);
+    }
+    void creditsClick()
+    {
+        SceneManager.LoadScene("Credits", LoadSceneMode.Single);
+    }
+}
--- Unity Project/ElementalShowdown/Assets/Code/Startup.cs
+++ Unity Project/ElementalShowdown/Assets/Code/Startup.cs
@@ -5,16 +5,52 @@ using UnityEngine.SceneManagement;
 
 public class Startup : MonoBehaviour
 {
+    public enum Track
+    {
+        Main, Game, Victory
+    }
+
+
+    [SerializeField]
+    private AudioSource audioPlayer;
+    [SerializeField]
+    private AudioClip mainTrack;
+    [SerializeField]
+    private AudioClip gameTrack;
+    [SerializeField]
+    private AudioClip endTrack;
+
+    private static AudioSource AudioPlayer;
+    private static AudioClip MainTrack;
+    private static AudioClip GameTrack;
+    private static AudioClip EndTrack;
+
     // Start is called before the first frame update
     void Start()
     {
+        AudioPlayer = audioPlayer;
+        MainTrack = mainTrack;
+        GameTrack = gameTrack;
+        EndTrack = endTrack;
+
+        DontDestroyOnLoad(gameObject);
         // Load the main menu
-        //SceneManager.LoadScene()
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
     }
 
-    // Update is called once per frame
-    void Update()
+    public static void ChangeAudioTrack(Track track)
     {
-        
+        switch (track)
+        {
+            case Track.Game:
+                AudioPlayer.clip = GameTrack;
+                break;
+            case Track.Main:
+                AudioPlayer.clip = MainTrack;
+                break;
+            case Track.Victory:
+                AudioPlayer.clip = EndTrack;
+                break;
+        }
     }
 }
