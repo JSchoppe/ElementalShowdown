20af33fff720da126ab45c2ef464c4eefc14f1c3
Ishan Dutta <ishan.dutta2006@yahoo.co.in>
2019-04-13 14:31:50 -0500

    Credits scene

---
+++ Unity Project/ElementalShowdown/Assets/Code/Credits.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu", UnityEngine.SceneManagement.LoadSceneMode.Single);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
